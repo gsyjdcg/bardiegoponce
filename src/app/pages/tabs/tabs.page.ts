70265dad250cdfd0fa4056a0a00a301e993d94b1
José Derick Cuesta González <derick@herbecon.es>
2021-02-21 13:18:37 +0100

    cambios al seleccionar los tabs

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
+export class TabsPage{
+  selected = 'raciones';
+
   constructor() {}
+
+  setSelectedTab(tab: string): void {
+    this.selected = tab;
+  }
 }
