e52da9de5e41968d12b4feff79851bfde1f84924
José Derick Cuesta González <derick@herbecon.es>
2021-08-03 23:18:08 +0200

    Actualizar tapas

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,15 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonTabs } from '@ionic/angular';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage{
+export class TabsPage {
   selected = 'raciones';
-
-  constructor() {}
 
   setSelectedTab(tab: string): void {
     this.selected = tab;
